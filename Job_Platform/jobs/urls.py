f4647db04fb8f2a65312c26dacf77c3b15508f03
James <qjaymce37@gmail.com>
2025-11-29 11:00:06 +0000

    modify endpoints in jobs/urls.py to ensure reverse

--- Job_Platform/jobs/urls.py
+++ Job_Platform/jobs/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from .views import IndustryViewSet, LocationViewSet, CompanyViewSet, JobViewSet
 
 router = DefaultRouter()
-router.register("industries", IndustryViewSet)
-router.register("locations", LocationViewSet)
-router.register("companies", CompanyViewSet)
-router.register("", JobViewSet)
+router.register("industries", IndustryViewSet, basename="industry")
+router.register("locations", LocationViewSet, basename="location")
+router.register("companies", CompanyViewSet, basename="company")
+router.register("", JobViewSet, basename="jobs")
 
 urlpatterns = [path("", include(router.urls))]
