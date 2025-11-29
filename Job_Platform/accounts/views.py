4688d3309028646b0ca1ada75aca958d1d480a83
James <qjaymce37@gmail.com>
2025-11-29 09:50:43 +0000

    modify views.py in accounts

--- Job_Platform/accounts/views.py
+++ Job_Platform/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer, UserProfileSerializer
+from .models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
