109ad0f835bb46cb8351f485fe17d7d7394122ff
James <qjaymce37@gmail.com>
2025-11-29 10:55:23 +0000

    modify views.py in accounts for reverse lookups to work

--- Job_Platform/accounts/urls.py
+++ Job_Platform/accounts/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterView, MeView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),
-    path("login/", TokenObtainPairView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(),name="login"),
     path("token/refresh/", TokenRefreshView.as_view()),
-    path("me/", MeView.as_view()),
+    path("me/", MeView.as_view(), name="me"),
 ]
