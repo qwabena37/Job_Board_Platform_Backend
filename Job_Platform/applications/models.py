d46a72e4c38eab9874459330a32ea628f69bddbd
James <qjaymce37@gmail.com>
2025-11-29 10:17:08 +0000

    modify models.py in applications app

--- Job_Platform/applications/models.py
+++ Job_Platform/applications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from jobs.models import Job
 
-# Create your models here.
+class ApplyJob(models.Model):
+    applicant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    cover_letter = models.TextField(blank=True)
+    resume_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
