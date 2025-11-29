dea0227f4bf80cb031559374b8bd646999fabd3a
James <qjaymce37@gmail.com>
2025-11-29 10:14:29 +0000

    modify views.py in jobs app

--- Job_Platform/jobs/views.py
+++ Job_Platform/jobs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Industry, Location, Company, Job
+from .serializers import IndustrySerializer, LocationSerializer, CompanySerializer, JobSerializer
 
-# Create your views here.
+class IndustryViewSet(viewsets.ModelViewSet):
+    queryset = Industry.objects.all()
+    serializer_class = IndustrySerializer
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(posted_by=self.request.user)
