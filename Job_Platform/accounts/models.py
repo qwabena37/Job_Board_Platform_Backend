dec8d52c5df160117b66c3046caf5455e3c5ec27
James <qjaymce37@gmail.com>
2025-11-29 09:45:25 +0000

    build accounts app authentication

--- Job_Platform/accounts/models.py
+++ Job_Platform/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (('admin', 'Admin'), ('user', 'User'))
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    def is_admin(self):
+        return self.role == "admin" or self.is_staff
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
