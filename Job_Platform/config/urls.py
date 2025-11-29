3aff4605ca1b81d7d1d330d41fd540c14040dc96
James <qjaymce37@gmail.com>
2025-11-29 10:26:43 +0000

    add url paths in config/urls.py

--- Job_Platform/config/urls.py
+++ Job_Platform/config/urls.py
@@ -29,4 +29,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/jobs/", include("jobs.urls")),
+    path("api/applications/", include("applications.urls")),
+    path("api/docs/", schema_view.with_ui("swagger")),
+
 ]
