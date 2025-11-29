5c2d1897b7a8918a4f9e9a7ba220d4dff9066457
James <qjaymce37@gmail.com>
2025-11-29 10:18:57 +0000

    modify views.py in applications app

--- Job_Platform/applications/views.py
+++ Job_Platform/applications/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import ApplyJob
+from .serializers import ApplicationSerializer
 
-# Create your views here.
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = ApplyJob.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(applicant=self.request.user)
