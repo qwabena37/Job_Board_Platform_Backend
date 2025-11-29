1856849d577054eda29359f6261ff7d4da81f9d7
James <qjaymce37@gmail.com>
2025-11-29 09:56:30 +0000

    add JWT login in views.py in accounts

--- Job_Platform/accounts/views.py
+++ Job_Platform/accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .serializers import RegisterSerializer, UserProfileSerializer
 from .models import User
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
