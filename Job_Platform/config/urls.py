75aec48e895b4498733a72af9a59b457aaf77a12
James <qjaymce37@gmail.com>
2025-11-29 10:23:37 +0000

    add swagger documentations in config/urls.py

--- Job_Platform/config/urls.py
+++ Job_Platform/config/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(title="ALX Project Nexus API", default_version="v1"),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
