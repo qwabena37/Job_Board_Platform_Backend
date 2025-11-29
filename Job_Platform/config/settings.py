6122675e6871af6ec658dcf58db00772f4aa039c
James <qjaymce37@gmail.com>
2025-11-29 09:42:17 +0000

    add REST Framework configurations in config/settings.py

--- Job_Platform/config/settings.py
+++ Job_Platform/config/settings.py
@@ -129,4 +129,20 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL ="aacounts.User"
+AUTH_USER_MODEL ="accounts.User"
+
+REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ),
+        "DEFAULT_PERMISSION_CLASSES": (
+            'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        ),
+        "DEFAULT_FILTER_BACKENDS": [
+            'django_filters.rest_framework.DjangoFilterBackend',
+            'rest_framework.filters.SearchFilter',
+            'rest_framework.filters.OrderingFilter',
+        ],
+        "PAGE_SIZE": 12,
+
+}
