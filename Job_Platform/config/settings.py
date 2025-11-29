43d18ab26cb6d8484f3c14404c3fd7e76829be6b
James <qjaymce37@gmail.com>
2025-11-29 11:39:00 +0000

    modify pagination class in config/settings.py

--- Job_Platform/config/settings.py
+++ Job_Platform/config/settings.py
@@ -144,6 +144,7 @@ REST_FRAMEWORK = {
             'rest_framework.filters.SearchFilter',
             'rest_framework.filters.OrderingFilter',
         ],
-        "PAGE_SIZE": 12,
+         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 
 }
