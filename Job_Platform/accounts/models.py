ca8c0a8f5d36e1ca24fa074448d5297c9fae1df7
James <qjaymce37@gmail.com>
2025-11-29 11:20:47 +0000

    add reciever decorator in models.py in accounts to automatically generate profile

--- Job_Platform/accounts/models.py
+++ Job_Platform/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     ROLE_CHOICES = (('admin', 'Admin'), ('user', 'User'))
@@ -13,3 +15,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(blank=True)
     phone = models.CharField(max_length=30, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
